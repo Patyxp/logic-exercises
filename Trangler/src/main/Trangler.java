fa2fd9aa95a4e7d5c65c44c7e89fca99d7bec39c
FIC <FIC@LAB11-WS01.info.local>
2018-06-19 21:45:42 -0300

    Projeto Triangler

---
+++ Trangler/src/main/Trangler.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Trangler {
+	
+	public static void main(String[] args) {
+ 		String letraA = "a";
+		for (int i = 0; i < 4; i++) {
+		letraA = letraA + "a";
+		System.out.println (letraA);
+	  }
+		
+				
+	}
+
+}
+	
+		
+
